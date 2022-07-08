d2ba4c915b02f10a64b2245884e3d32f974aa623
Thanh Le <99621274+Sokiben93@users.noreply.github.com>
2022-07-08 15:16:35 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $buttonNext.click(() => {
     $wrapper.empty();
     $wrapper1.empty();
     $wrapper2.empty();
-    $.get(`https://randomuser.me/api/?page=${currPage}&results=1`, (data) => {
+    $.get(`https://randomuser.me/api/?page=${currPage}&results=3`, (data) => {
         console.log(data);
         // $wrapper.innerHTML = '';
         data.results.forEach((search) => {
@@ -38,68 +38,6 @@ $buttonNext.click(() => {
             console.log(div1);
         });
     });
-
-    $.get(`https://randomuser.me/api/?page=${currPage + 1}&results=1`, (data) => {
-        console.log(data);
-        // $wrapper.innerHTML = '';
-        data.results.forEach((search) => {
-            let div1 = $('<div class="person1">');
-            div1.appendTo($wrapper1);
-            // div1.appendTo($wrapper1);
-            // div1.appendTo($wrapper2);
-            let div2 = $('<div class="header-info">');
-            div2.appendTo(div1);
-            let img = $(`<img class="image" src=${search.picture.large}></img>`);
-            img.appendTo(div2);
-            let div3 = $(`<div class="info">`);
-            div3.appendTo(div1);
-            let div4 = $(`<div class="name">${search.name.title}. ${search.name.first}</div>`);
-            div4.appendTo(div3);
-            let div5 = $(`<div class="country">${search.location.country}</div>`);
-            div5.appendTo(div3);
-            let div6 = $('<div class="footer-info">');
-            div6.appendTo(div1);
-            let div7 = $('<div class="more">');
-            div7.appendTo(div6);
-            let span = $(`<span class="gender">${search.gender}</span>`);
-            span.appendTo(div7);
-            let span1 = $(`<span class="age">Age: ${search.registered.age}</span>`);
-            span1.appendTo(div7);
-            let div8 = $(`<div class="email">${search.email}</div>`);
-            div8.appendTo(div6);
-            console.log(div1);
-        });
-    })
-
-    $.get(`https://randomuser.me/api/?page=${currPage + 2}&results=1`, (data) => {
-        console.log(data);
-        // $wrapper.innerHTML = '';
-        data.results.forEach((search) => {
-            let div1 = $('<div class="person2">');
-            div1.appendTo($wrapper2);
-            let div2 = $('<div class="header-info">');
-            div2.appendTo(div1);
-            let img = $(`<img class="image" src=${search.picture.large}></img>`);
-            img.appendTo(div2);
-            let div3 = $(`<div class="info">`);
-            div3.appendTo(div1);
-            let div4 = $(`<div class="name">${search.name.title}. ${search.name.first}</div>`);
-            div4.appendTo(div3);
-            let div5 = $(`<div class="country">${search.location.country}</div>`);
-            div5.appendTo(div3);
-            let div6 = $('<div class="footer-info">');
-            div6.appendTo(div1);
-            let div7 = $('<div class="more">');
-            div7.appendTo(div6);
-            let span = $(`<span class="gender">${search.gender}</span>`);
-            span.appendTo(div7);
-            let span1 = $(`<span class="age">Age: ${search.registered.age}</span>`);
-            span1.appendTo(div7);
-            let div8 = $(`<div class="email">${search.email}</div>`);
-            div8.appendTo(div6);
-            console.log(div1);
-        });
-    })
     currPage++;
 })
 
@@ -137,8 +75,6 @@ const $button = $('#submit');
 // currPage++;
 $button.click(() => {
     $wrapper.empty();
-    $wrapper1.empty();
-    $wrapper2.empty();
     // var userInput = $("input:text").val();
     var userNat = $("input:text").val();
     // $.get(`https://randomuser.me/api/?gender=${userInput.toLowerCase()}`, (data) => {
@@ -173,73 +109,7 @@ $button.click(() => {
     //     });
     // });
 
-    // $.get(`https://randomuser.me/api/?gender=${userInput.toLowerCase()}`, (data) => {
-    //     console.log(data);
-    //     // $wrapper.innerHTML = '';
-    //     data.results.forEach((search) => {
-    //         let div1 = $('<div class="person">');
-    //         // div1.appendTo($wrapper);
-    //         div1.appendTo($wrapper1);
-    //         // div1.appendTo($wrapper2);
-    //         let div2 = $('<div class="header-info">');
-    //         div2.appendTo(div1);
-    //         let img = $(`<img class="image" src=${search.picture.large}></img>`);
-    //         img.appendTo(div2);
-    //         let div3 = $(`<div class="info">`);
-    //         div3.appendTo(div1);
-    //         let div4 = $(`<div class="name">${search.name.title}. ${search.name.first}</div>`);
-    //         div4.appendTo(div3);
-    //         let div5 = $(`<div class="country">${search.location.country}</div>`);
-    //         div5.appendTo(div3);
-    //         let div6 = $('<div class="footer-info">');
-    //         div6.appendTo(div1);
-    //         let div7 = $('<div class="more">');
-    //         div7.appendTo(div6);
-    //         let span = $(`<span class="gender">${search.gender}</span>`);
-    //         span.appendTo(div7);
-    //         let span1 = $(`<span class="age">Age: ${search.registered.age}</span>`);
-    //         span1.appendTo(div7);
-    //         let div8 = $(`<div class="email">${search.email}</div>`);
-    //         div8.appendTo(div6);
-    //         console.log(div1);
-    //     });
-    // });
-
-    // $.get(`https://randomuser.me/api/?gender=${userInput.toLowerCase()}`, (data) => {
-    //     console.log(data);
-    //     // $wrapper.innerHTML = '';
-    //     data.results.forEach((search) => {
-    //         let div1 = $('<div class="person">');
-    //         // div1.appendTo($wrapper);
-    //         // div1.appendTo($wrapper1);
-    //         div1.appendTo($wrapper2);
-    //         let div2 = $('<div class="header-info">');
-    //         div2.appendTo(div1);
-    //         let img = $(`<img class="image" src=${search.picture.large}></img>`);
-    //         img.appendTo(div2);
-    //         let div3 = $(`<div class="info">`);
-    //         div3.appendTo(div1);
-    //         let div4 = $(`<div class="name">${search.name.title}. ${search.name.first}</div>`);
-    //         div4.appendTo(div3);
-    //         let div5 = $(`<div class="country">${search.location.country}</div>`);
-    //         div5.appendTo(div3);
-    //         let div6 = $('<div class="footer-info">');
-    //         div6.appendTo(div1);
-    //         let div7 = $('<div class="more">');
-    //         div7.appendTo(div6);
-    //         let span = $(`<span class="gender">${search.gender}</span>`);
-    //         span.appendTo(div7);
-    //         let span1 = $(`<span class="age">Age: ${search.registered.age}</span>`);
-    //         span1.appendTo(div7);
-    //         let div8 = $(`<div class="email">${search.email}</div>`);
-    //         div8.appendTo(div6);
-    //         console.log(div1);
-    //     });
-    // });
-
-
-
-    $.get(`https://randomuser.me/api/?nat=${userNat.toUpperCase()}`, (data) => {
+    $.get(`https://randomuser.me/api/?nat=${userNat.toUpperCase()}&results=3`, (data) => {
         console.log(data);
         // $wrapper.innerHTML = '';
         data.results.forEach((search) => {
@@ -247,70 +117,6 @@ $button.click(() => {
             div1.appendTo($wrapper);
             // div1.appendTo($wrapper1);
             // div1.appendTo($wrapper2);
-            let div2 = $('<div class="header-info">');
-            div2.appendTo(div1);
-            let img = $(`<img class="image" src=${search.picture.large}></img>`);
-            img.appendTo(div2);
-            let div3 = $(`<div class="info">`);
-            div3.appendTo(div1);
-            let div4 = $(`<div class="name">${search.name.title}. ${search.name.first}</div>`);
-            div4.appendTo(div3);
-            let div5 = $(`<div class="country">${search.location.country}</div>`);
-            div5.appendTo(div3);
-            let div6 = $('<div class="footer-info">');
-            div6.appendTo(div1);
-            let div7 = $('<div class="more">');
-            div7.appendTo(div6);
-            let span = $(`<span class="gender">${search.gender}</span>`);
-            span.appendTo(div7);
-            let span1 = $(`<span class="age">Age: ${search.registered.age}</span>`);
-            span1.appendTo(div7);
-            let div8 = $(`<div class="email">${search.email}</div>`);
-            div8.appendTo(div6);
-            console.log(div1);
-        });
-    });
-
-    $.get(`https://randomuser.me/api/?nat=${userNat.toUpperCase()}`, (data) => {
-        console.log(data);
-        // $wrapper.innerHTML = '';
-        data.results.forEach((search) => {
-            let div1 = $('<div class="person">');
-            // div1.appendTo($wrapper);
-            div1.appendTo($wrapper1);
-            // div1.appendTo($wrapper2);
-            let div2 = $('<div class="header-info">');
-            div2.appendTo(div1);
-            let img = $(`<img class="image" src=${search.picture.large}></img>`);
-            img.appendTo(div2);
-            let div3 = $(`<div class="info">`);
-            div3.appendTo(div1);
-            let div4 = $(`<div class="name">${search.name.title}. ${search.name.first}</div>`);
-            div4.appendTo(div3);
-            let div5 = $(`<div class="country">${search.location.country}</div>`);
-            div5.appendTo(div3);
-            let div6 = $('<div class="footer-info">');
-            div6.appendTo(div1);
-            let div7 = $('<div class="more">');
-            div7.appendTo(div6);
-            let span = $(`<span class="gender">${search.gender}</span>`);
-            span.appendTo(div7);
-            let span1 = $(`<span class="age">Age: ${search.registered.age}</span>`);
-            span1.appendTo(div7);
-            let div8 = $(`<div class="email">${search.email}</div>`);
-            div8.appendTo(div6);
-            console.log(div1);
-        });
-    });
-
-    $.get(`https://randomuser.me/api/?nat=${userNat.toUpperCase()}`, (data) => {
-        console.log(data);
-        // $wrapper.innerHTML = '';
-        data.results.forEach((search) => {
-            let div1 = $('<div class="person">');
-            // div1.appendTo($wrapper);
-            // div1.appendTo($wrapper1);
-            div1.appendTo($wrapper2);
             let div2 = $('<div class="header-info">');
             div2.appendTo(div1);
             let img = $(`<img class="image" src=${search.picture.large}></img>`);
